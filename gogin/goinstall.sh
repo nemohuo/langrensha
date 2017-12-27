7542fc1de455d65e8aaaf8b2330f787deee1bb58
nemohuo <huoqiangqiang@shopex.cn>
2017-12-27 18:03:39 +0800

    增加文件

---
+++ gogin/goinstall.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+appname=$1;
+CURRENT_DIR=`pwd`
+OLD_GO_PATH="$GOPATH"  #例如: /usr/local/go
+OLD_GO_BIN="$GOBIN"    #例如: /usr/local/go/bin
+
+export GOPATH="$CURRENT_DIR" 
+export GOBIN="$CURRENT_DIR/bin"
+
+#指定并整理当前的源码路径
+gofmt -w src
+
+go install $appname
+
+export GOPATH="$OLD_GO_PATH"
+export GOBIN="$OLD_GO_BIN"
